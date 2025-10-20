d2a849664e154dc971924a4eb9472d83a64cfecb
Cuchibambo.P <m.aubin.iribarren@gmail.com>
2025-10-20 20:13:55 +0200

    commit on me

--- python/algoworks/betteralgo.py
+++ python/algoworks/betteralgo.py
@@ -1,6 +1,48 @@
 import random
 import json
-import tkinter
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.configure(bg="#2b2b2b") 
+frm = ttk.Frame(root, padding=10, style="Custom.TFrame")
+frm.grid()
+frm.configure(style="Custom.TFrame")
+frm.pack(fill="both", expand=True)
+style = ttk.Style()
+style.theme_use("clam")
+style.configure("Custom.TFrame", background="#2b2b2b")
+
+style.configure(
+    "Custom.TButton",
+    background="#454545",
+    foreground="white",
+    font=("Segoe UI", 11, "bold"),
+    padding=10,
+    borderwidth=0,
+    focusthickness=0,
+    focuscolor="none",
+    relief="flat"
+)
+
+style.map(
+    "Custom.TButton",
+    background=[("active", "#357ABD"), ("pressed", "#2E5E9E")]
+)
+
+style.configure(
+    "Custom.TLabel",
+    background="#2b2b2b",
+    foreground="white",
+    font=("Segoe UI", 12, 'bold')
+)
+
+style.configure(
+    "Category.TLabel",
+    background="#2b2b2b",
+    foreground="#bababa",
+    font=("Segoe UI", 9)
+)
 
 class video():
     def __init__(self, titlefirstword, titlesecondword, titlethirdword):
@@ -19,64 +61,118 @@ class video():
 
 data={}
 
+# firstwordcategories = {
+#     "Mastering": "Education",
+#     "Exploring": "Entertainment",
+#     "Unveiling": "Education",
+#     "Discovering": "Entertainment",
+#     "Creating": "Education",
+#     "Transforming": "Education",
+#     "Understanding": "Education",
+#     "Navigating": "Education",
+#     "Building": "DIY & How-To",
+#     "Enhancing": "DIY & How-To"
+# }
+
 firstwordcategories = {
-    "Mastering" : "Education",
-    "Exploring" : "Travel",
-    "Unveiling" : "Entertainment",
-    "Discovering" : "Education",
-    "Creating" : "DIY & How-To",
-    "Transforming" : "DIY & How-To",
-    "Understanding" : "Education",
-    "Navigating" : "Travel",
-    "Building" : "DIY & How-To",
-    "Enhancing" : "DIY & How-To"
+    "Apprendre" : "Éducation",
+    "Explorer" : "Divertissement",
+    "Découvrir": "Divertissement",
+    "Transformé": "Éducation",
+    "Comprendre" : "Éducation",
+    "Entreprendre" : "Affaires et finances",
+    "Construire" : "Bricolage et tutoriels",
+    "Améliorer": "Bricolage et tutoriels"
 }
+
+# secondwordcategories = {
+#     "Digital Marketing Strategies" : "Business & Finance",
+#     "The Human Brain" : "Science",
+#     "Artificial Intelligence" : "Technology",
+#     "Culinary Arts" : "Food & Cooking",
+#     "Personal Finance" : "Business & Finance",
+#     "Travel Destinations" : "Travel",
+#     "Fitness Routines" : "Health & Fitness",
+#     "Photography Techniques" : "DIY & How-To",
+#     "Home Decor Ideas" : "DIY & How-To",
+#     "Mindfulness Practices" : "Health & Fitness"
+# }
 
 secondwordcategories = {
-    "Digital Marketing Strategies" : "Business & Finance",
-    "The Human Brain" : "Science",
-    "Artificial Intelligence" : "Technology",
-    "Culinary Arts" : "Food & Cooking",
-    "Personal Finance" : "Business & Finance",
-    "Travel Destinations" : "Travel",
-    "Fitness Routines" : "Health & Fitness",
-    "Photography Techniques" : "DIY & How-To",
-    "Home Decor Ideas" : "DIY & How-To",
-    "Mindfulness Practices" : "Health & Fitness"
+    "Le vélo": "Santé et fitness",
+    "La natation": "Santé et fitness",
+    "Les jeux vidéo": "Divertissement",
+    "La musique": "Divertissement",
+    "Le dessin": "Divertissement",
+    "La photographie": "Divertissement",
+    "Le cinéma": "Divertissement",
+    "La cuisine": "Nourriture et cuisine"
 }
 
+# thirdwordcategories = {
+#     "for Beginners" : "Education",
+#     "on a Budget" : "Business & Finance",
+#     "in 10 Minutes" : "DIY & How-To",
+#     "You Need to Know" : "Education",
+#     "Like a Pro" : "DIY & How-To",
+#     "Step-by-Step" : "DIY & How-To",
+#     "for this year" : "Education",
+#     "That Will Change Your Life" : "Motivation & Inspiration",
+#     "Revealed" : "Education",
+#     "Made Easy" : "DIY & How-To"
+# }
+
 thirdwordcategories = {
-    "for Beginners" : "Education",
-    "on a Budget" : "Business & Finance",
-    "in 10 Minutes" : "DIY & How-To",
-    "You Need to Know" : "Education",
-    "Like a Pro" : "DIY & How-To",
-    "Step-by-Step" : "DIY & How-To",
-    "for this year" : "Education",
-    "That Will Change Your Life" : "Motivation & Inspiration",
-    "Revealed" : "Education",
-    "Made Easy" : "DIY & How-To"
+    "pour les débutants": "Éducation",
+    "à petit budget": "Affaires et finances",
+    "en 10 minutes": "Bricolage et tutoriels",
+    "que vous devez savoir": "Éducation",
+    "comme un pro": "Bricolage et tutoriels",
+    "étape par étape": "Bricolage et tutoriels",
+    "cette année": "Éducation",
+    "qui va changer votre vie": "Motivation et inspiration",
+    "révélé": "Éducation",
+    "facilement": "Bricolage et tutoriels"
 }
 
 def makingData():
+    # data = {
+    #     "preferredfirstword": {
+    #         "Mastering": 0, "Exploring": 0, "Unveiling": 0, "Discovering": 0, "Creating": 0,
+    #         "Transforming": 0, "Understanding": 0, "Navigating": 0, "Building": 0, "Enhancing": 0
+    #     },
+    #     "preferredsecondword": {
+    #         "Digital Marketing Strategies": 0, "The Human Brain": 0, "Artificial Intelligence": 0,
+    #         "Culinary Arts": 0, "Personal Finance": 0, "Travel Destinations": 0, "Fitness Routines": 0,
+    #         "Photography Techniques": 0, "Home Decor Ideas": 0, "Mindfulness Practices": 0
+    #     },
+    #     "preferredthirdword": {
+    #         "for Beginners": 0, "on a Budget": 0, "in 10 Minutes": 0, "You Need to Know": 0,
+    #         "Like a Pro": 0, "Step-by-Step": 0, "for this year": 0, "That Will Change Your Life": 0,
+    #         "Revealed": 0, "Made Easy": 0
+    #     },
+    #     "preferredcategorie": {
+    #         "Education": 0, "Travel": 0, "Entertainment": 0, "DIY & How-To": 0, "Business & Finance": 0, "Science": 0, "Technology": 0,
+    #         "Food & Cooking": 0, "Health & Fitness": 0, "Motivation & Inspiration": 0 
+    #     },
+    # }
     data = {
         "preferredfirstword": {
-            "Mastering": 0, "Exploring": 0, "Unveiling": 0, "Discovering": 0, "Creating": 0,
-            "Transforming": 0, "Understanding": 0, "Navigating": 0, "Building": 0, "Enhancing": 0
+            "Apprendre" : 0, "Explorer" : 0, "Découvrir": 0, "Transformé": 0, "Comprendre" : 0, "Entreprendre" : 0,
+            "Construire" : 0, "Améliorer": 0
         },
         "preferredsecondword": {
-            "Digital Marketing Strategies": 0, "The Human Brain": 0, "Artificial Intelligence": 0,
-            "Culinary Arts": 0, "Personal Finance": 0, "Travel Destinations": 0, "Fitness Routines": 0,
-            "Photography Techniques": 0, "Home Decor Ideas": 0, "Mindfulness Practices": 0
+            "Le vélo": 0, "La natation": 0, "Les jeux vidéo": 0, "La musique": 0, "Le dessin": 0,
+            "La photographie": 0, "Le cinéma": 0, "La cuisine": 0
         },
         "preferredthirdword": {
-            "for Beginners": 0, "on a Budget": 0, "in 10 Minutes": 0, "You Need to Know": 0,
-            "Like a Pro": 0, "Step-by-Step": 0, "for this year": 0, "That Will Change Your Life": 0,
-            "Revealed": 0, "Made Easy": 0
+            "pour les débutants": 0, "à petit budget": 0, "en 10 minutes": 0, "que vous devez savoir": 0,
+            "comme un pro": 0, "étape par étape": 0, "cette année": 0, "qui va changer votre vie": 0,
+            "révélé": 0, "facilement": 0
         },
         "preferredcategorie": {
-            "Education": 0, "Travel": 0, "Entertainment": 0, "DIY & How-To": 0, "Business & Finance": 0, "Science": 0, "Technology": 0,
-            "Food & Cooking": 0, "Health & Fitness": 0, "Motivation & Inspiration": 0 
+            "Éducation": 0, "Santé et fitness": 0, "Divertissement": 0, "Bricolage et tutoriels": 0,
+            "Affaires et finances": 0, "Motivation et inspiration": 0, "Nourriture et cuisine": 0
         },
     }
     return data
@@ -85,9 +181,12 @@ data = makingData()
 
 def makeVideos():
     
-    titlefirstword = ["Mastering","Exploring","Unveiling","Discovering","Creating","Transforming","Understanding","Navigating","Building","Enhancing"]
-    titlesecondword = ["Digital Marketing Strategies","The Human Brain","Artificial Intelligence","Culinary Arts","Personal Finance","Travel Destinations","Fitness Routines","Photography Techniques","Home Decor Ideas","Mindfulness Practices"]
-    titlethirdword = ["for Beginners","on a Budget","in 10 Minutes","You Need to Know","Like a Pro","Step-by-Step","for this year","That Will Change Your Life","Revealed","Made Easy"]
+    # titlefirstword = ["Mastering","Exploring","Unveiling","Discovering","Creating","Transforming","Understanding","Navigating","Building","Enhancing"]
+    # titlesecondword = ["Digital Marketing Strategies","The Human Brain","Artificial Intelligence","Culinary Arts","Personal Finance","Travel Destinations","Fitness Routines","Photography Techniques","Home Decor Ideas","Mindfulness Practices"]
+    # titlethirdword = ["for Beginners","on a Budget","in 10 Minutes","You Need to Know","Like a Pro","Step-by-Step","for this year","That Will Change Your Life","Revealed","Made Easy"]
+    titlefirstword = ["Apprendre","Explorer","Découvrir","Transformé","Comprendre","Entreprendre","Construire","Améliorer"]
+    titlesecondword = ["Le vélo","La natation","Les jeux vidéo","La musique","Le dessin", "La photographie", "Le cinéma", "La cuisine"]
+    titlethirdword = ["pour les débutants","à petit budget","en 10 minutes","que vous devez savoir","comme un pro","étape par étape","cette année","qui va changer votre vie","révélé","facilement"]
     
     videos = []
     for _ in range(10):
@@ -172,39 +271,66 @@ def LoadData():
 watchbonus = 1
 skippenalty = -1/3
 
-def VideoInteractions(selectedVideo):
-    while True:
-        watchorskip = input("To watch type the number corresponding to the desisred video: ").strip()
-        if watchorskip == "1":
-            changeCurrentScore(watchbonus,selectedVideo[0])
-            changeCurrentScore(skippenalty,selectedVideo[1])
-            changeCurrentScore(skippenalty,selectedVideo[2])
-            MainLoop()
-            break
-        elif watchorskip == "2":
-            changeCurrentScore(skippenalty,selectedVideo[0])
-            changeCurrentScore(watchbonus,selectedVideo[1])
-            changeCurrentScore(skippenalty,selectedVideo[2])
-            MainLoop()
-            break
-        elif watchorskip == "3":
-            changeCurrentScore(skippenalty,selectedVideo[0])
-            changeCurrentScore(skippenalty,selectedVideo[1])
-            changeCurrentScore(watchbonus,selectedVideo[2])
-            MainLoop()
-            break
-        elif watchorskip == "save":
-            SaveData()
-        elif watchorskip == "load":
-            LoadData()
-        else:
-            print("Invalid input")
+# def VideoInteractions(selectedVideo,watchorskip):
+#     while True:
+#         watchorskip = input("To watch type the number corresponding to the desisred video: ").strip()
+#         if watchorskip == 0:
+#             changeCurrentScore(watchbonus,selectedVideo[0])
+#             changeCurrentScore(skippenalty,selectedVideo[1])
+#             changeCurrentScore(skippenalty,selectedVideo[2])
+#             return 0
+#         elif watchorskip == 1:
+#             changeCurrentScore(skippenalty,selectedVideo[0])
+#             changeCurrentScore(watchbonus,selectedVideo[1])
+#             changeCurrentScore(skippenalty,selectedVideo[2])
+#             return 0
+#         elif watchorskip == 2:
+#             changeCurrentScore(skippenalty,selectedVideo[0])
+#             changeCurrentScore(skippenalty,selectedVideo[1])
+#             changeCurrentScore(watchbonus,selectedVideo[2])
+#             return 0
+
+def FirstVideoSelected(bestvideos):
+    changeCurrentScore(watchbonus,bestvideos[0])
+    changeCurrentScore(skippenalty,bestvideos[1])
+    changeCurrentScore(skippenalty,bestvideos[2])
+    MainLoop()
+    
+def SecondVideoSelected(bestvideos):
+    changeCurrentScore(skippenalty,bestvideos[0])
+    changeCurrentScore(watchbonus,bestvideos[1])
+    changeCurrentScore(skippenalty,bestvideos[2])
+    MainLoop()
+    
+def ThirdVideoSelected(bestvideos):
+    changeCurrentScore(skippenalty,bestvideos[0])
+    changeCurrentScore(skippenalty,bestvideos[1])
+    changeCurrentScore(watchbonus,bestvideos[2])
+    MainLoop()
+    
+def SkippedAll(bestvideos):
+    changeCurrentScore(skippenalty,bestvideos[0])
+    changeCurrentScore(skippenalty,bestvideos[1])
+    changeCurrentScore(skippenalty,bestvideos[2])
+    MainLoop()
 
 def MainLoop():
+    for widget in frm.winfo_children():
+        widget.destroy()
     bestvideos = FindBestVideo()
-    VideoInfo(bestvideos[0])
-    VideoInfo(bestvideos[1])
-    VideoInfo(bestvideos[2])
-    VideoInteractions(bestvideos)
+    ttk.Label(frm, text="Video 1", style="Custom.TLabel").grid(column=0, row=0)
+    ttk.Label(frm, text="Video 2", style="Custom.TLabel").grid(column=1, row=0)
+    ttk.Label(frm, text="Video 3", style="Custom.TLabel").grid(column=2, row=0)
+    ttk.Button(frm, text=f'{bestvideos[0].titlefirstword}\n{bestvideos[0].titlesecondword}\n{bestvideos[0].titlethirdword}',style="Custom.TButton", command=lambda: FirstVideoSelected(bestvideos)).grid(column=0, row=1, padx=10, pady=10)
+    ttk.Button(frm, text=f'{bestvideos[1].titlefirstword}\n{bestvideos[1].titlesecondword}\n{bestvideos[1].titlethirdword}',style="Custom.TButton", command=lambda: SecondVideoSelected(bestvideos)).grid(column=1, row=1, padx=10, pady=10)
+    ttk.Button(frm, text=f'{bestvideos[2].titlefirstword}\n{bestvideos[2].titlesecondword}\n{bestvideos[2].titlethirdword}',style="Custom.TButton", command=lambda: ThirdVideoSelected(bestvideos)).grid(column=2, row=1, padx=10, pady=10)
+    ttk.Label(frm, text=f"Categorie: {bestvideos[0].category}", style="Category.TLabel").grid(column=0, row=2)
+    ttk.Label(frm, text=f"Categorie: {bestvideos[1].category}", style="Category.TLabel").grid(column=1, row=2)
+    ttk.Label(frm, text=f"Categorie: {bestvideos[2].category}", style="Category.TLabel").grid(column=2, row=2)
+    ttk.Button(frm, text='Save',style="Custom.TButton", command=lambda: SaveData()).grid(column=0, row=3, pady=10)
+    ttk.Button(frm, text='Load',style="Custom.TButton", command=lambda: LoadData()).grid(column=1, row=3, pady=10)
+    ttk.Button(frm, text='Quit',style="Custom.TButton", command=root.destroy).grid(column=2, row=3, pady=10)
+    ttk.Button(frm, text='Skip All',style="Custom.TButton", command=lambda: SkippedAll(bestvideos)).grid(column=1, row=4, padx=10, pady=10)
 
 MainLoop()
+root.mainloop()
