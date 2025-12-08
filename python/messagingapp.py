cfc687064249b99fa465a4c24eab987776aa6faa
Cuchibambo.P <m.aubin.iribarren@gmail.com>
2025-12-08 09:24:22 +0100

    fzs<

--- python/messagingapp.py
+++ python/messagingapp.py
@@ -1,35 +1,25 @@
 import random
-import json
-from tkinter import *
-from tkinter import ttk
-
-# Start and setup tkinter
-root = Tk()
-root.configure(bg="#2b2b2b") 
-frm = ttk.Frame(root, padding=10, style="Custom.TFrame")
-frm.grid()
-frm.configure(style="Custom.TFrame")
-frm.pack(fill="both", expand=True)
-style = ttk.Style()
-style.theme_use("clam")
-style.configure("Custom.TFrame", background="#2b2b2b")
-
-# Button Style
-style.configure(
-    "Custom.TButton",
-    background="#454545",
-    foreground="white",
-    font=("Segoe UI", 11, "bold"),
-    padding=10,
-    borderwidth=0,
-    focusthickness=0,
-    focuscolor="none",
-    relief="flat"
-)
+import math
 
 class user():
     def __init__(self, username, password):
         self.username = username
         self.passwords = password
-        
-    
+        message_history = []
+
+user1 = user('Maurice', 'Cuchibambo')
+
+block_size = 3
+
+def CBCencryption(text_to_encrypt:str,key:int,vi) -> str:
+    text_to_encrypt = text_to_encrypt.strip()
+
+splitString('hello')
+
+def toBinary(a:str):
+    l,m=[],[]
+    for i in a:
+        l.append(ord(i))
+    for i in l:
+        m.append(int(bin(i)[2:]))
+    return m
